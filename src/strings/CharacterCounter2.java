870a9b0b23af8bf04491524f6758861a1fc909ba
vspaz <vladimir.edelman@gmail.com>
2020-12-23 00:08:40 +0100

    add alternative solution

--- src/strings/CharacterCounter2.java
+++ src/strings/CharacterCounter2.java
@@ -1,4 +1,17 @@
 package strings;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class CharacterCounter2 {
+    public static void main(String[] args) {
+        Map<Character, Integer> charToCount = new TreeMap<>();
+        String someString = "foobarbazzzzfufufuf";
+        for (char c : someString.toCharArray()) {
+            charToCount.compute(c, (key, value) -> (value == null) ? 1 : ++ value);
+        }
+        for (Map.Entry<Character, Integer> characterToCount : charToCount.entrySet()) {
+            System.out.printf("%s => %s\n", characterToCount.getKey(), characterToCount.getValue());
+        }
+    }
 }
