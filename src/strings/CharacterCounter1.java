9cb0ec5a1b3fd7d2c2af3b30f97ebf76d95f50e6
vspaz <vladimir.edelman@gmail.com>
2020-12-22 23:48:37 +0100

    minor improvements

--- src/strings/CharacterCounter1.java
+++ src/strings/CharacterCounter1.java
@@ -9,14 +9,14 @@ public class CharacterCounter1 {
         Map<Character, Integer> charToCount = new TreeMap<>();
         for (char character : someString.toCharArray()) {
             if (charToCount.containsKey(character)) {
-                int count = charToCount.get(character) + 1;
-                charToCount.put(character, count);
+                int count = charToCount.get(character);
+                charToCount.put(character, ++count);
             } else {
                 charToCount.put(character, 1);
             }
         }
         for (Map.Entry<Character, Integer> characterToCount : charToCount.entrySet()) {
-            System.out.println(String.format("%s => %s", characterToCount.getKey(), characterToCount.getValue()));
+            System.out.printf("%s => %s\n", characterToCount.getKey(), characterToCount.getValue());
         }
     }
 }
