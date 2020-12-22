9636d8ad379fae13209b5a56aa0c3eaa1e838fd4
vspaz <vladimir.edelman@gmail.com>
2020-12-22 23:59:17 +0100

    minor fix

--- src/strings/CharacterCounter1.java
+++ src/strings/CharacterCounter1.java
@@ -3,9 +3,9 @@ package strings;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class CharacterCounter1 {
-    public static void main(String[] args) {
-        String someString = "foobarbazzzzfufufuf";
+
+class Counter {
+    static void countChars(String someString) {
         Map<Character, Integer> charToCount = new TreeMap<>();
         for (char character : someString.toCharArray()) {
             if (charToCount.containsKey(character)) {
@@ -18,5 +18,12 @@ public class CharacterCounter1 {
         for (Map.Entry<Character, Integer> characterToCount : charToCount.entrySet()) {
             System.out.printf("%s => %s\n", characterToCount.getKey(), characterToCount.getValue());
         }
+    }
+}
+
+public class CharacterCounter1 {
+    public static void main(String[] args) {
+        String someString = "foobarbazzzzfufufuf";
+        Counter.countChars(someString);
     }
 }
