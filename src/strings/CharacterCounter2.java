adacf2719ce2a3cc4472126b23b79a7468fbc893
vspaz <vladimir.edelman@gmail.com>
2020-12-23 00:38:17 +0100

    refactor class 2

--- src/strings/CharacterCounter2.java
+++ src/strings/CharacterCounter2.java
@@ -3,19 +3,20 @@ package strings;
 import java.util.Map;
 import java.util.TreeMap;
 
-
-public class CharacterCounter2 {
-    public static void main(String[] args) {
-        String someString = "foobarbazzzzfufufuf";
-        Map<Character, Integer> charToInteger = countChars(someString);
-        StringUtils.print(charToInteger);
-    }
-
-    static Map<Character, Integer> countChars(String someString) {
+class Couter2 extends Counter {
+    static Map<Character, Integer> countChars() {
         Map<Character, Integer> charToCount = new TreeMap<>();
         for (char c : someString.toCharArray()) {
             charToCount.compute(c, (key, value) -> (value == null) ? 1 : ++value);
         }
         return charToCount;
+    }
+
+}
+
+public class CharacterCounter2 {
+    public static void main(String[] args) {
+        Map<Character, Integer> charToInteger = Couter2.countChars();
+        StringUtils.print(charToInteger);
     }
 }
