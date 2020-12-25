ceac2d43896fbc5521fea25701d93697687876c9
vspaz <vladimir.edelman@gmail.com>
2020-12-25 23:16:45 +0100

    renaming

--- tests/strings/FirstNonRepeatedCharacterTest.java
+++ tests/strings/FirstNonRepeatedCharacterTest.java
@@ -5,13 +5,13 @@ import org.testng.Assert;
 
 public class FirstNonRepeatedCharacterTest {
     @Test
-    public void testSingleNonRepeatedCharacter_1() {
+    public void testFirstAndOnlyNonRepeatedCharacter() {
         Character firstNonRepeatedCharacter = Repetition.getFirstNonRepeatedCharacter("bbaacczaa");
         Assert.assertEquals(firstNonRepeatedCharacter, (Character) 'z');
     }
 
     @Test
-    public void testSingleNonRepeatedCharacter_2() {
+    public void testFirstNonRepeatedCharacterOutOfMany() {
         Character firstNonRepeatedCharacter = Repetition.getFirstNonRepeatedCharacter("bbaacclaamnn");
         Assert.assertEquals(firstNonRepeatedCharacter, (Character) 'l');
     }
