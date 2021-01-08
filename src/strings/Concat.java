21f426919c37ef1010391fed1c89fe7cae2c50a9
vspaz <vladimir.edelman@gmail.com>
2021-01-08 21:34:06 +0100

    add a separator to method; add tests

--- src/strings/Concat.java
+++ src/strings/Concat.java
@@ -3,10 +3,13 @@ package strings;
 import java.util.ArrayList;
 
 public class Concat {
-    public static String concat(ArrayList<String> words) {
+    public static String concat(ArrayList<String> words, String sep) {
         StringBuilder sb = new StringBuilder();
-        for (String word : words) {
-            sb.append(word);
+        for (int i = 0; i < words.size(); i++) {
+            sb.append(words.get(i));
+            if (i < words.size() - 1) {
+                sb.append(sep);
+            }
         }
         return sb.toString();
     }
--- tests/strings/ConcatTest.java
+++ tests/strings/ConcatTest.java
@@ -14,6 +14,23 @@ public class ConcatTest {
         words.add("bar");
         words.add("baz");
 
-        Assert.assertEquals("foobarbaz", Concat.concat(words));
+        Assert.assertEquals("foobarbaz", Concat.concat(words, ""));
+    }
+
+    @Test
+    void testConcatWithSeparator() {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("foo");
+        words.add("bar");
+        words.add("baz");
+
+        Assert.assertEquals("foo.bar.baz", Concat.concat(words, "."));
+    }
+
+    @Test
+    void testConcatWithSeparatorAndSingleElement() {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("foo");
+        Assert.assertEquals("foo", Concat.concat(words, "."));
     }
 }
