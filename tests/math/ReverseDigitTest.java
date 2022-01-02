a38338edc096924487e999c8271b37c46c1b574c
vspaz <vladimir.edelman@gmail.com>
2022-01-02 15:51:39 +0100

    add test 'testReverseDigitOk'

--- tests/math/ReverseDigitTest.java
+++ tests/math/ReverseDigitTest.java
@@ -1,5 +1,12 @@
 package math;
 
-public class ReverseDigitTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReverseDigitTest {
+    @Test
+    void testReverseDigitOk() {
+        assertEquals(1234, ReversedDigit.reverseDigit(4321));
+    }
 }
