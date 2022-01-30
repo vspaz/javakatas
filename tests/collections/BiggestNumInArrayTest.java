757a5eff905d91a3fe867f7eea63b5c1780d3494
vspaz <vladimir.edelman@gmail.com>
2022-01-30 19:44:58 +0100

    extend tests

--- tests/collections/BiggestNumInArrayTest.java
+++ tests/collections/BiggestNumInArrayTest.java
@@ -9,4 +9,14 @@ public class BiggestNumInArrayTest {
     Assert.assertEquals(
         140, BiggestNumInArray.getBiggestNum(new int[] {5, 3, 10, 2, -5, 8, 140, 80, 11}));
   }
+
+  @Test
+  void testEmptyArray() {
+    Assert.assertThrows(() -> BiggestNumInArray.getBiggestNum(new int[0]));
+  }
+
+  @Test
+  void testArrayWithSingleElement() {
+    Assert.assertEquals(10, BiggestNumInArray.getBiggestNum(new int[] {10}));
+  }
 }
