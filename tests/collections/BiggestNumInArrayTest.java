d15971f7b9fd7b8fd193b49a8ffce1fa65d25d5b
vspaz <vladimir.edelman@gmail.com>
2022-01-30 19:37:02 +0100

    add test case

--- tests/collections/BiggestNumInArrayTest.java
+++ tests/collections/BiggestNumInArrayTest.java
@@ -1,4 +1,12 @@
 package collections;
 
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
 public class BiggestNumInArrayTest {
+  @Test
+  void testGetBiggestNum() {
+    Assert.assertEquals(
+        140, BiggestNumInArray.getBiggestNum(new int[] {5, 3, 10, 2, -5, 8, 140, 80, 11}));
+  }
 }
